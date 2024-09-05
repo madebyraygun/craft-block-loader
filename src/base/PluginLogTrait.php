2130a0500e413913ff482d4a4a588f142d61eb28
Dalton Rooney <dalton@madebyraygun.com>
2024-09-05 06:43:45 -0700

    feat: add logger support

---
+++ src/base/PluginLogTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace madebyraygun\blockloader\base;
+
+use Craft;
+use craft\log\MonologTarget;
+use madebyraygun\blockloader\Plugin;
+use Monolog\Formatter\LineFormatter;
+use Psr\Log\LogLevel;
+
+trait PluginLogTrait
+{
+    public static function log(string $message, array $params = []): void
+    {
+        $message = Craft::t('block-loader', $message, $params);
+        Craft::info($message, __METHOD__);
+    }
+
+    public static function error(string $message, array $params = []): void
+    {
+        $message = Craft::t('block-loader', $message, $params);
+        Craft::error($message, __METHOD__);
+    }
+
+    public static function warning(string $message, array $params = []): void
+    {
+        $message = Craft::t('block-loader', $message, $params);
+        Craft::warning($message, __METHOD__);
+    }
+
+    public static function registerMonologTargetLevel($level): void
+    {
+        $target = new MonologTarget([
+            'name' => Plugin::$pluginHandle,
+            'categories' => ['madebyraygun\blockloader\*'],
+            'level' => $level,
+            'logContext' => false,
+            'allowLineBreaks' => true,
+            'logVars' => ['_GET', '_POST'],
+            'formatter' => new LineFormatter(
+                format: "%datetime% [%level_name%] %message%\n",
+                dateFormat: 'Y-m-d H:i:s',
+                allowInlineLineBreaks: true,
+                ignoreEmptyContextAndExtra: true,
+            ),
+        ]);
+        Craft::getLogger()->dispatcher->targets[] = $target;
+    }
+
+    public static function registerLogger(): void
+    {
+        // self::registerMonologTargetLevel(LogLevel::ERROR);
+        self::registerMonologTargetLevel(LogLevel::INFO);
+    }
+}
