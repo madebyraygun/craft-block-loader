29cda46c2982058b9bf9ef697b3db0cb90412441
Dalton Rooney <dalton@madebyraygun.com>
2024-09-10 15:09:21 -0700

    fix: matrix->contentId replaced by entry->id

--- src/base/ContextDescriptor.php
+++ src/base/ContextDescriptor.php
@@ -13,13 +13,13 @@ class ContextDescriptor
     public string $blockHandle;
     public array $context;
 
-    public function __construct(ContextBlock $block, Entry $matrixBlock)
+    public function __construct(ContextBlock $block, Entry $matrixEntry)
     {
         $block = clone $block;
-        $context = $block->getContext($matrixBlock);
-        $this->matrixId = $matrixBlock->contentId;
-        $this->blockHandle = $matrixBlock->type->handle;
-        $this->order = $matrixBlock->sortOrder;
+        $context = $block->getContext($matrixEntry);
+        $this->matrixId = $matrixEntry->id;
+        $this->blockHandle = $matrixEntry->type->handle;
+        $this->order = $matrixEntry->sortOrder;
         $this->handle = $block->settings->contextHandle;
         $this->cacheable = $block->settings->cacheable;
         $this->context = $context;
